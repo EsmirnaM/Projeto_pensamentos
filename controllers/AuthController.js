c44df0411df9f0df32fbb9be99c0d2350f51c490
Esmirna Marino Aragon <esmirnaamneris@gmail.com>
2023-04-26 19:12:43 -0300

    continuando com registro e flash message

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,3 +1,8 @@
+const User = require('../models/User')
+
+const bcrypt = require('bcryptjs')
+
+
 module.exports = class AuthController{
     static login(req, res) {
         res.render('auth/login')
@@ -5,5 +10,21 @@ module.exports = class AuthController{
 
     static register(req, res) {
         res.render('auth/register')
+    };
+
+    static async registerPost(req, res) {
+       
+        const {name, email, password, confirmpassword } = req.body
+
+        // Validação do password
+
+        if (password != confirmpassword) {
+            // mensagem
+            req.flash('message', 'As senhas não conferem, porfavor verifique e tente novamente')
+            res.render('auth/register')
+
+            return
+            
+        }
     };
 }   
