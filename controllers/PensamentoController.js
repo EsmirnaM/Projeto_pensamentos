32123b0ce6cd2a1213ff93dc6666b91a27f5bbf6
Esmirna Marino Aragon <esmirnaamneris@gmail.com>
2023-04-30 15:24:30 -0300

    configurando Dashboard

--- controllers/PensamentoController.js
+++ controllers/PensamentoController.js
@@ -6,4 +6,14 @@ module.exports = class PensamentoController {
     static async showPensamentos(req, res){
         res.render('pensamentos/home')
     }
+
+    static async dashboard(req, res) {
+        res.render('pensamentos/dashboard')
+    }
+
+    static createPensamento(req, res) {
+        res.render('pesamentos/create')
+    }
+
+
 }
